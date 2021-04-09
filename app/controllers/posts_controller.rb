0a20f8147481e47fc39a3365a25c4f11416ce3b1
Ryan Erricson <ryanerricson@gmail.com>
2021-04-09 18:28:16 -0400

    commit tree 899b7c31ad6f29e8e8fedddd2d4f22bfba013fe8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    @post = Post.find_by_id(params[:id])
+    if @post.update(post_params) && @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
